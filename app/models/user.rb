06f2794957bbc6534660c2983f15241f692969f3
Hal Tang <hal.wh.tang@gmail.com>
2017-03-16 21:12:13 +0000

    Added form for user registration and writes to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -57,12 +57,12 @@ ActiveRecord::Schema.define(version: 20170316192210) do
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name",      null: false
-    t.string   "last_name",       null: false
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "first_name", null: false
+    t.string   "last_name",  null: false
+    t.string   "email",      null: false
+    t.string   "password",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "line_items", "orders"
