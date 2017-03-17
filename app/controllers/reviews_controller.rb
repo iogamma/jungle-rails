040ca4c1ced4d08e31b0e50ddc486a237e04e177
Hal Tang <hal.wh.tang@gmail.com>
2017-03-17 23:48:22 +0000

    Submitted form and saved to database

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,27 @@
 class ReviewsController < ApplicationController
   def create
+    @parent = Product.find_by_id params[:product_id]
 
+    if current_user
+      @review = Review.new({
+        product_id: params[:product_id],
+        user_id: current_user.id,
+        description: review_params[:description],
+        rating: review_params[:rating]
+      })
+      if @review.save
+        redirect_to @parent
+      else
+        redirect_to @parent
+      end
+    else
+      redirect_to @parent
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:product_id,
+                                   :description,
+                                   :rating)
   end
 end
