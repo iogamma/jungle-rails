ee9674c7b9ead886729ffd38f2d1750a0c7f9300
Hal Tang <hal.wh.tang@gmail.com>
2017-03-18 00:29:37 +0000

    Added a before filter to ReviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,27 +1,22 @@
 class ReviewsController < ApplicationController
-  def create
-    @parent = Product.find_by_id params[:product_id]
+  before_action :authorize
 
-    if current_user
-      @review = Review.new({
-        product_id: params[:product_id],
-        user_id: current_user.id,
-        description: review_params[:description],
-        rating: review_params[:rating]
-      })
-      if @review.save
-        redirect_to @parent
-      else
-        redirect_to @parent
-      end
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to @product, notice: "Your review has been posted."
     else
-      redirect_to @parent
+      redirect_to @product, alert: "Sorry, we could not save your review. Please contact support."
     end
   end
 
+  private
+
   def review_params
-    params.require(:review).permit(:product_id,
-                                   :description,
+    params.require(:review).permit(:description,
                                    :rating)
   end
 end
