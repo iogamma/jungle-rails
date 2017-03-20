d7b554dd0a75a50e5133f25c5d81b6e5aea5b296
Hal Tang <hal.wh.tang@gmail.com>
2017-03-20 02:10:52 +0000

    Redefined login, logout and register routes to /login, /logout, and /register. Formated login form and added error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,36 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by_email(user_params["email"])
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    if user && user.authenticate(user_params["password"])
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to root_path
     else
     # If user's login doesn't work, tell them
     # and send them back to the login form.
-      # TODO: Give user a failed login message
-      redirect_to '/user/login'
+      flash[:alert] = "Your login credentials not matching"
+      redirect_to login_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to :back
+  rescue ActionController::RedirectBackError
+    redirect_to root_path
   end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:email,
+                                   :password)
+    end
 end
 
