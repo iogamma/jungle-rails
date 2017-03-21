75747f7b128e13049f1e50cfe1c43718c8710c2d
Hal Tang <hal.wh.tang@gmail.com>
2017-03-21 22:45:11 +0000

    Wrote test for adding product to cart, and refactored the other two feature tessts

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "AddToCarts", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -10,20 +9,23 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
-	      image: open_asset('apparel1.jpg'),
+        image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
     end
   end
 
-  scenario "They see all products" do
+  scenario "They see product detail page" do
     # ACT
     visit root_path
+    page.first('article.product').find_link(text: 'Add').click
 
     # DEBUG / VERIFY
-    save_screenshot
-    expect(page).to have_css 'article.product', count: 10
+    # wait long enough for screen capture
+    # sleep 2
+    # save_screenshot
+    within('.navbar') { expect(page).to have_content('My Cart (1)') }
   end
 
 end
--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel1.jpg'),
+        image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
@@ -17,15 +17,15 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
   end
 
  scenario "They see product detail page" do
-  # ACT
-  visit root_path
-  page.find_link(@category.products.first.name).click
+    # ACT
+    visit root_path
+    page.find_link(@category.products.first.name).click
 
-  # DEBUG / VERIFY
-  # wait long enough for screen capture
-  # sleep 2
-  # save_screenshot
-  expect(page.has_css?('.product-detail'))
-  expect(page).to have_content(@category.products.first.name)
+    # DEBUG / VERIFY
+    # wait long enough for screen capture
+    # sleep 2
+    # save_screenshot
+    expect(page.has_css?('.product-detail'))
+    expect(page).to have_content(@category.products.first.name)
   end
 end
